9ab205be992afa0c56f671deae74830b006fddf2
brahman1 <brahim-arab@outlook.fr>
2026-08-17 15:22:52 +0200

    fix: os is not defined in auto updater

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 const YtDlpManager = require('./yt-dlp-manager');
 
 class SimpleStore {
